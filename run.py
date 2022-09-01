2c9b3728ad20f20bcf86091b9cf1fd803cb4fb82
Ruslan <66064451+DarkPrinceWarrior@users.noreply.github.com>
2022-09-01 08:16:35 +0300

    Maria DB configured via SqlAlchemy

--- run.py
+++ run.py
@@ -1,8 +1,26 @@
 from app.controllers import selectAll
 from app.models import db_setup
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
 
-if __name__ == '__main__':
+
+def main():
     db_setup.init_db()
-    # Show all employees
+    # Show all users
     print('All users')
     selectAll()
+
+
+def app_gui():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Hello")
+    window.setGeometry(300, 250, 350, 200)
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+    app_gui()
